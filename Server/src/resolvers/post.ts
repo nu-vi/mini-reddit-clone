0ab4f50969a696a7719b96ff593b0b4aa3145a6d
Nuno Vinagreiro <nunocmavinagreiro@gmail.com>
2021-04-06 18:09:17 +0100

    CRUD opearions with mikro-ORM & type-graphQL for a basic entity done

---
+++ Server/src/resolvers/post.ts
@@ -0,0 +1,62 @@
+import { MyContext } from 'src/types';
+import { Arg, Ctx, Int, Mutation, Query, Resolver } from 'type-graphql';
+import { Post } from '../entities/Post';
+
+@Resolver()
+export class PostResolver {
+  @Query(() => [Post])
+  posts(@Ctx() { em }: MyContext): Promise<Post[]> {
+    return em.find(Post, {});
+  }
+
+  @Query(() => Post, { nullable: true })
+  post(
+    @Arg('id', () => Int) id: number, //in this case the second argument of @Arg isn't really necessary as type-grahpql can infer it is an id because of the typescript type
+    @Ctx() { em }: MyContext
+  ): Promise<Post | null> {
+    return em.findOne(Post, { id });
+  }
+
+  @Mutation(() => Post)
+  async createPost(
+    @Arg('title') title: string,
+    @Ctx() { em }: MyContext
+  ): Promise<Post> {
+    const post = em.create(Post, { title });
+
+    await em.persistAndFlush(post);
+    return post;
+  }
+
+  @Mutation(() => Post, { nullable: true })
+  async updatePost(
+    @Arg('id') id: number,
+    @Arg('title', () => String, { nullable: true }) title: string,
+    @Ctx() { em }: MyContext
+  ): Promise<Post | null> {
+    const post = await em.findOne(Post, { id });
+
+    if (!post) {
+      return null;
+    }
+    if (typeof title !== 'undefined') {
+      post.title = title;
+      await em.persistAndFlush(post);
+    }
+    return post;
+  }
+
+  @Mutation(() => Boolean)
+  async deletePost(
+    @Arg('id') id: number,
+    @Ctx() { em }: MyContext
+  ): Promise<boolean> {
+    try {
+      await em.nativeDelete(Post, { id });
+    } catch(err) {
+      console.error(err);
+      return false;
+    }
+    return true;
+  }
+}
